55b85a0be06ad7b86153b819616e2dedf270fc53
Pankaj4828 <pankajbehera4828@gmail.com>
2026-05-06 05:54:55 +0530

    Added file test matrix strategy

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return 'This is a simple Flask application to demonstrate GitHub Actions.'
